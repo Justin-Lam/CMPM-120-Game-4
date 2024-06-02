3915f617d6e50ed64ec1390a0dff42617b7744d0
blytheSchen <blschen@ucsc.edu>
2024-06-01 19:36:44 -0700

    implemented temporary map with collisions and camera movement
    implement temporary map with collisions (between player and map and enemy and map, no enemy1 and map yet) and camera movement

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -14,6 +14,14 @@ class Load extends Phaser.Scene
 		this.load.image("Net Swipe", "Net Swipe_temp.png");
 		this.load.image("Bread", "Bread_temp.png");
 		this.load.image("Enemy", "Enemy_temp.png");
+
+		// Load map tilesets
+		this.load.image("urban_tiles", "urban_tilemap_packed_bigger.png"); 
+		this.load.image("rpg_tiles", "roguelikeSheet_transparent.png"); 
+		this.load.image("shmup_tiles", "shmup_tiles_packed.png"); 
+		this.load.image("town_tiles", "town_tilemap_packed.png"); 
+
+		this.load.tilemapTiledJSON("basic-level-alt", "basic-level-alt.tmj");
 	}
 
 	create()
