6827d03fa15b6542db4383ed40a1cfe4821c6e63
blytheSchen <blschen@ucsc.edu>
2024-06-03 18:52:25 -0700

    Working randomgen for flooring

--- src/Game Objects/Tile.js
+++
@@ -1,9 +0,0 @@
-class Tile extends Phaser.GameObjects.Sprite {
-    constructor(scene, x, y, key) 
-    {
-        super(scene, x, y, key);
-        this.scene = scene;
-        this.scene.add.existing(this);
-        this.setOrigin(0);
-    }
-}
--- src/Scenes/Test_MapGen.js
+++ src/Scenes/Test_MapGen.js
@@ -10,30 +10,37 @@ class Test_MapGen extends Phaser.Scene
         this.mapWidth = 50;
         this.mapHeight = 20; // In tiles
 
-        const TILES = 
+        this.TILES = 
         {
+            FLOOR: [
+                {index: 0, weight: 2.5},
+                {index: 1, weight: 2.5},
+                {index: 2, weight: 3},
+                {index: 3, weight: 1},
+                {index: 4, weight: 0.5},
+            ]
         } // Contains information on what each layer's possible tile images are along with weight (likelihood of being chosen)
     }
 
     create()
 	{
-        // Create a blank tilemap with dimensions
-        this.map = this.make.tilemap({ tileWidth: 16, tileHeight: 16, width: this.mapWidth, height: this.mapHeight });
+        // Create a blank tilemap
+        this.map = this.make.tilemap({tileWidth: 16, tileHeight: 16, width: this.mapWidth, height: this.mapHeight});
 
-        // Add tilesets to the map
-        this.tilesetList = [
-            this.map.addTilesetImage("urban_tilemap_packed_bigger", "urban_tiles"), 
-			this.map.addTilesetImage("roguelikeSheet_transparent", "rpg_tiles"), 
-			this.map.addTilesetImage("shmup_tiles_packed", "shmup_tiles"), 
-			this.map.addTilesetImage("town_tilemap_packed", "town_tiles") 
-        ];
+        this.floorTileset = this.map.addTilesetImage("flooring-tileset", "floor_tiles"), 
+        console.log(this.floorTileset);
 
-        // Make layer with tileset list
-        this.floorLayer = this.map.createBlankLayer('Floor', this.tilesetList);
+        // Make layer with tileset lists
+        this.floorLayer = this.map.createBlankLayer('Floor', this.floorTileset);
+        this.floorLayer.setScale(SCALE);
 
-        // The probability of any index being picked is (the indexs weight) / (sum of all weights)
-        weightedRandomize(weightedIndexes, [layer]);
-    	// Map randomgen tester (assigned to Q key)
+        // From Phaser docs: The probability of any index being picked is (the indexs weight) / (sum of all weights)
+        // Fill the floor with mostly clean tiles, but occasionally place a dirty tile
+        // See "Weighted Randomize" example for more information on how to use weightedRandomize.
+        console.log(this.mapWidth);
+        this.map.weightedRandomize(this.TILES.FLOOR, 0, 0, this.mapWidth, this.mapHeight, 'Floor');
+
+        // Map randomgen tester (assigned to Q key)
 		this.input.keyboard.on('keydown-Q', () => {
 			console.log("switched to game test scene");
 			this.scene.start("testScene");
